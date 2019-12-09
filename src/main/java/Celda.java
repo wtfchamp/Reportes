86c5360d3a34dc771eb0514448be79e5c5bab1ba
Jesus <sauronavp92@hotmail.com>
2019-12-09 16:16:33 -0600

    Reportes primer commit

---
+++ src/main/java/Celda.java
@@ -0,0 +1,17 @@
+
+public class Celda {
+    int largo;
+    int ancho;
+    String texto;
+
+    public Celda(int largo, int ancho, String texto){
+        this.largo = largo;
+        this.ancho = ancho;
+        this.texto = texto;
+    }
+
+
+    public void creaCelda(){
+        
+    }
+}
